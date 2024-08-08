2ab85b0541117f96d5b8319738d21c378753b2cb
Harry Lee <harrileeeee@gmail.com>
2024-08-08 13:52:09 -0700

    fix(cicd): build time env

--- deploy.sh
+++ deploy.sh
@@ -12,13 +12,6 @@ else
     echo "Container cpt-fe does not exist. Creating a new one."
 fi
 
-# set environment variables
-export NEXT_PUBLIC_BACKEND_URL="https://shnyu.danlanlove.com/api"
-export NEXT_PUBLIC_CONVERSATION_GAP="2500"
-export NEXT_PUBLIC_PROJECT_NAME="上海纽约大学压力与健康研究"
-export NEXT_PUBLIC_SHUFFLE_ARRAY_FLAG="true"
-
-
 # Start a new container with the latest image
 docker run \
     -e "NEXT_PUBLIC_BACKEND_URL=$NEXT_PUBLIC_BACKEND_URL" \
