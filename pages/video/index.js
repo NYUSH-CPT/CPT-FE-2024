3a22d89f8c3da5d8f4a5ee3a8c43c724d5fe2d29
YongyuLiu03 <yl8126@nyu.edu>
2024-09-05 20:57:10 -0500

    issue #19, #20, #22

--- pages/video/index.js
+++ pages/video/index.js
@@ -6,22 +6,26 @@ import Head from "next/head";
 import Header from '@/components/Header'
 
 import { Button, Card } from '@mui/material';
-import ReactPlayer from 'react-player';
+import moment from 'moment'
 
 
 const Video = () => {
     const videoRef = useRef(null)
     const [watched, setWatched] = useState(false)
-    const [played, setPlayed] = useState(0)
+    const [currentTime, setCurrentTime] = useState('')
+    const [playPauseText, setPlayPauseText] = useState('播放')
 
-    const [isClent, setIsClient] = useState(false)
+
     useEffect(() => {
-        setIsClient(true)
-        const watched = localStorage.getItem(`__autosave-${window.location.pathname}-watched`)
-        if (watched) {
-            setWatched(true)
-            console.log("video watched from local storage")
-        }
+        requester.get("/info").then((res) => {
+            const currentDay = res.data.currentDay
+            console.log("currentDay", currentDay)
+            if (currentDay >= 2.1) {
+                setWatched(true)
+            }
+        }).catch((err) => {
+            console.log(err)
+        })
     }, [])
 
 
@@ -36,31 +40,48 @@ const Video = () => {
             <h1>第2天 科普视频</h1>
             <p>视频播放完毕后将自动更新首页任务列表</p>
             <Card>
-                {isClent && <ReactPlayer
-                    height="100%"
-                    width="100%"
-                    url="/video.mp4"
-                    controls
+                <video
+                    src="/video.mp4"
                     ref={videoRef}
-                    onProgress={() => {
-                        videoRef.current.getCurrentTime() >= played && 
-                        setPlayed(videoRef.current.getCurrentTime())
-                    }}
-                    onSeek={() => {
-                        !watched && videoRef.current.getCurrentTime() > played && 
-                        videoRef.current.seekTo(played)
-                    }}
                     onEnded={() => {
-                        if (videoRef.current.getCurrentTime()>=videoRef.current.getDuration()) {
-                            console.log("video end")
-                            requester.post('/video')
-                            localStorage.setItem(`__autosave-${window.location.pathname}-watched`, true)
+                        if (!watched && videoRef.current.currentTime >= videoRef.current.duration) {
+                            console.log("video end");
+                            requester.post('/video');
+                            setWatched(true);
                         }
+                        setPlayPauseText('重新播放')
                     }}
-                />}
+                    onTimeUpdate={() => {
+                        const currentMoment = moment.duration(videoRef.current.currentTime, 'seconds')
+                        const durationMoment = moment.duration(videoRef.current.duration, 'seconds')
+                        setCurrentTime(
+                            `${currentMoment.minutes() + ':' + currentMoment.seconds()}/${
+                                durationMoment.minutes() + ':' + durationMoment.seconds()
+                            }`
+                        )
+                    }}
+                />
+
             </Card>
             <div 
                 className={styles.videoControl}>
+                    <span className={styles.videoTime}>{currentTime}</span>
+                    <Button
+                        color='primary'
+                        variant='contained'
+                        onClick={() => {
+                            if (videoRef.current.paused) {
+                                videoRef.current.play()
+                                setPlayPauseText('暂停')
+                            } else {
+                                videoRef.current.pause()
+                                setPlayPauseText('播放')
+                            }
+                        }}
+                    >
+                        {playPauseText}
+                    </Button>
+
                     <Button 
                         color='primary'
                         variant='contained'
