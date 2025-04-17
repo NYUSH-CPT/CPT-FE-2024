e8bd6076a5aff1e969fd9208db3c1f2cf2710aec
YongyuLiu03 <yl8126@nyu.edu>
2025-04-17 14:49:39 -0500

    update qualtrics link

--- text.js
+++ text.js
@@ -203,7 +203,7 @@ export const TASK_EXP_GRP = [
         title: '第1天：问卷调查',
         description: '预计时间：30分钟',
         completed_description: '已完成',
-        url: 'https://nyu.qualtrics.com/jfe/form/SV_0pmbmG3UcATIhPU',
+        url: 'https://nyu.qualtrics.com/jfe/form/SV_e9BDbHNLDURjDg2',
         completed_url: '/'
     },
     {   
@@ -284,7 +284,7 @@ export const TASK_EXP_GRP = [
         title: '第23天：问卷调查',
         description: '预计时间：30分钟',
         completed_description: '已完成',
-        url: 'https://nyu.qualtrics.com/jfe/form/SV_eS7IRMfy2rUKvbg',
+        url: 'https://nyu.qualtrics.com/jfe/form/SV_b4m9463r8Hsf1xc',
         completed_url: '/'
     }
 ]
@@ -295,7 +295,7 @@ export const TASK_WL_GRP = [
         title: '第1天：问卷调查',
         description: '预计时间：30分钟',
         completed_description: '已完成',
-        url: 'https://nyu.qualtrics.com/jfe/form/SV_bfqaJJoB84pUy4C',
+        url: 'https://nyu.qualtrics.com/jfe/form/SV_54RK735erfNve5g',
         completed_url: '/'
     },
     {
@@ -303,7 +303,7 @@ export const TASK_WL_GRP = [
         title: '第23天：问卷调查',
         description: '预计时间：30分钟',
         completed_description: '已完成',
-        url: 'https://nyu.qualtrics.com/jfe/form/SV_eS7IRMfy2rUKvbg',
+        url: 'https://nyu.qualtrics.com/jfe/form/SV_1M0Y9JrFaJZYow6',
         completed_url: '/'
     },
     {   
