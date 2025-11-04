f2f0ed4d0669c48fd0b5c684149c12c16aec257c
YongyuLiu03 <yl8126@nyu.edu>
2025-11-04 17:22:21 -0600

    update cicd

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,6 @@ docker run \
     -e "NEXT_PUBLIC_CONVERSATION_GAP=$NEXT_PUBLIC_CONVERSATION_GAP" \
     -e "NEXT_PUBLIC_PROJECT_NAME=$NEXT_PUBLIC_PROJECT_NAME" \
     -e "NEXT_PUBLIC_SHUFFLE_ARRAY_FLAG=$NEXT_PUBLIC_SHUFFLE_ARRAY_FLAG" \
-    -e "QR_JWT_SECRET=$QR_JWT_SECRET" \
     -d --name cpt-fe -p 3000:3000 ghcr.nju.edu.cn/nyush-cpt/cpt-fe-2024@$IMAGE_SHA
 
 docker system prune -f
