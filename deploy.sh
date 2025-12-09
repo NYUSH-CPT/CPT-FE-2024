39872b5de6db312097f11dc6262414e3c404d5fe
YongyuLiu03 <yl8126@nyu.edu>
2025-12-09 11:34:31 -0600

    update memory limit

--- deploy.sh
+++ deploy.sh
@@ -15,6 +15,7 @@ fi
 # Start a new container with the latest image
 docker run \
     --restart always \
+    --memory=300m \
     -e "NEXT_PUBLIC_BACKEND_URL=$NEXT_PUBLIC_BACKEND_URL" \
     -e "NEXT_PUBLIC_CONVERSATION_GAP=$NEXT_PUBLIC_CONVERSATION_GAP" \
     -e "NEXT_PUBLIC_PROJECT_NAME=$NEXT_PUBLIC_PROJECT_NAME" \
